1ebc0f6ad4b98c51dcc7fb0e90f9b0a73a0f433c
pi3r <pierre.nespo@gmail.com>
2013-07-18 10:01:49 -0700

    User multiple fields to find match against the regexp

--- app.js
+++ app.js
@@ -7,19 +7,35 @@
 
     initialize: function(data) {
       if (data.firstLoad){
-        var value = this.execRegexOnDescription();
-        var key = 'custom_field_' + this.setting('field_id');
+        var value = this.execRegexOnFields();
+        var key = 'custom_field_' + this.setting('result_field');
 
         if (value && _.isEmpty(this.ticket().customField(key))){
           this.ticket().customField(key, value);
         }
       }
-
     },
 
-    execRegexOnDescription: function(){
-      return (this.ticket().description().match(this.setting('regex')) || [])[1] || "";
-    }
+    execRegexOnFields: function(){
+      var value = '';
+
+      _.each(this.fields(), function(field){
+        if (!_.isEmpty(this.containerContext().ticket[field]) &&
+           _.isEmpty(value)){
+          value = (this.containerContext().ticket[field].match(this.regexp()) || [])[1] || "";
+        }
+      }, this);
+
+      return value;
+    },
+
+    fields: function(){
+      return _.compact(this.setting('fields').split(','));
+    },
+
+    regexp: _.memoize(function(){
+      return this.setting('regex');
+    })
   };
 
 }());
